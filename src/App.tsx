b437487e1139b5b8a3575517675c14c853e31455
bzoon <ben4808@gmail.com>
2025-05-21 01:40:28 -0600

    start adding routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
-import { BrowserRouter } from 'react-router';
+import { BrowserRouter, Outlet, Route, Routes, useLocation } from 'react-router-dom';
 import './App.css'
 import CrosswordList from './components/CrosswordList/CrosswordList'
 import Header from './components/Header/Header'
+import Solver from './components/Solver/Solver';
+
+// Layout component (combines Header and Outlet for content)
+function Layout() {
+  const location = useLocation();
+  return (
+    <div>
+      <Header isInSolver={location.pathname.includes("crossword") || location.pathname.includes("clue")} />
+      <Outlet /> {/* Child routes render here */}
+    </div>
+  );
+}
 
 function App() {
   return (
     <BrowserRouter>
-      <Header isInSolver={false}></Header>
-      <CrosswordList date={new Date()}></CrosswordList>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/list/:date" element={<CrosswordList date={undefined} />} />
+          <Route path="/crossword/:source/:date" element={<Solver />} />
+          <Route path="/clue/:id" element={<Solver />} />
+          <Route path="*" element={<CrosswordList />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 };
